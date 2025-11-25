863480b7fe08954de2106e93ccf4026d971f51d2
hamsu12 <hamsj2413@smail.kongju.ac.kr>
2025-11-25 10:48:56 +0900

    feat: initialize OST project

---
+++ src/main/java/com/example/ost/OstApplication.java
@@ -0,0 +1,13 @@
+package com.example.ost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ost/OstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
